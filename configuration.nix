1cbf8bd1484b9144207d45fabc1c73f9ba17d0e4
Waldir Borba Junior <wborbajr@gmail.com>
2025-01-16 11:11:41 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,16 @@
   # Enable flakes and nifx-command
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 2d";
+    };
+  };
+
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/vda";
